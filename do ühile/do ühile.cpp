1269745309bd6dcb45bd987fe7917c1588735595
mirze <mirze@DESKTOP-J4VSIP8>
2024-10-05 16:42:22 +0400

    Proje dosyası ekle.

---
+++ do ühile/do ühile.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int sola_surushdur(int eded) {
+    int temp = eded, uzunluq = 1, mertebe = 1;
+
+    while (temp >= 10) {
+        temp /= 10;
+        uzunluq++;
+        mertebe *= 10;
+    }
+
+    int sol_reqem = eded / mertebe;
+    int qalan_hisse = eded % mertebe;
+
+    return qalan_hisse * 10 + sol_reqem;
+}
+
+int main() {
+    cout << sola_surushdur(12345) << endl;
+    return 0;
+}
